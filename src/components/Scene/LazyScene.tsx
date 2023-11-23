2c37dfcb3a30395abdc81cb2a37178743e8cf949
jtgomba <jgomba@hotmail.co.uk>
2023-11-23 11:19:54 +0900

    perf: lazy loaded three canvas

--- src/components/Scene/LazyScene.tsx
+++ src/components/Scene/LazyScene.tsx
@@ -1,8 +1,12 @@
+'use client';
+
+import { useStore, StoreState } from '@/hooks/Store';
 import dynamic from 'next/dynamic';
 const Scene = dynamic(() => import('./BaseScene'));
 
 const LazyScene = () => {
-  return <Scene />;
+  const { started } = useStore((state: StoreState) => state);
+  return <>{started && <Scene />}</>;
 };
 
 export default LazyScene;
